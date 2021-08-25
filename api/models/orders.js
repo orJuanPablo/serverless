3a96270c5dc2e9bd1bcd79d19cb7ac0b61e6dd8e
juanPocampo <juanp.ocampo@hotmail.com>
2021-08-24 21:30:40 -0300

    debug

--- api/models/orders.js
+++ api/models/orders.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const Orders = mongoose.model('Order', new Schema(
     {
-        meal_id : {type : Schema.Types.ObjectId,},
+        meal_id : {type : Schema.Types.ObjectId, ref: 'Meal'},
         user_id : String,
     }
 ))
